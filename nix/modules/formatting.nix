63ab57cfde4740e0708e7a59eba09dca310fe8f9
Nikolai Schimke <schimkeNikolai@gmail.com>
2025-01-02 17:17:44 +0000

    Update flake configuration to include 'flake-root' input and enhance formatting module
    - Added 'flake-root' as a new input in flake.nix for improved project structure.
    - Updated formatting.nix to import 'flake-root' module, enhancing the formatting capabilities.

--- nix/modules/formatting.nix
+++ nix/modules/formatting.nix
@@ -2,13 +2,14 @@
 {
   imports = [
     inputs.treefmt-nix.flakeModule
+    inputs.flake-root.flakeModule
   ];
   perSystem = { config, self', pkgs, lib, ... }: {
     # Add your auto-formatters here.
     # cf. https://nixos.asia/en/treefmt
     treefmt.config = {
       projectRootFile = "flake.nix";
-      
+
       programs = {
         nixpkgs-fmt.enable = true;
         rustfmt.enable = true;
