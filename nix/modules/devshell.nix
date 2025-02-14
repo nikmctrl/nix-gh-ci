ddb89a263eca3765540d9f2ec1d756d79c5a7c08
Nikolai Schimke <schimkeNikolai@gmail.com>
2025-02-14 11:18:49 +0000

    Refactor flake configuration and add Cachix deploy support
    - Update flake.nix to use flake-utils and simplify system configuration
    - Add Cachix deploy flake and related dependencies
    - Modify crane setup for Rust project
    - Remove previous flake-parts configuration
    - Update README and remove devshell module
    - Adjust GitHub Actions workflow for SSH builder

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -1,18 +1,4 @@
 { inputs, ... }:
 {
-  perSystem = { config, self', pkgs, lib, ... }: {
-    devShells.default = pkgs.mkShell {
-      name = "rust-nix-gh-ci-example-shell";
-      inputsFrom = [
-        self'.devShells.rust
-        config.treefmt.build.devShell
-      ];
-      packages = with pkgs; [
-        just
-        nixd # Nix language server
-        bacon
-        cachix
-      ];
-    };
-  };
+  
 }
